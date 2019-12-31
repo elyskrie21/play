826ca417b0dbc7780c5cad8d10fbab5d8bcc86f9
Elyse Kriegel <esamkriegel@gmail.com>
2019-12-31 01:16:47 -0600

    Add the statics files to be used in express

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 
+var port = process.env.PORT || 3000;
+
+app.use("/public",express.static(__dirname + "/public")); 
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
 })
-
-
-var port = process.env.PORT || 3000;
 
 app.listen(port, function (req, res) {
     console.log("Listening to port " + port);
