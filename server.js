c3b5b24239b3bd8489fd0f257472b606c4d2dc8d
Elyse Kriegel <esamkriegel@gmail.com>
2020-01-02 00:56:24 -0600

    first commit for heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const request = require("request");
 
 var port = process.env.PORT || 3000;
 
-app.use("/public",express.static(__dirname + "/public")); 
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.urlencoded()); 
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/public/index.html");
@@ -12,3 +14,4 @@ app.get("/", function (req, res) {
 app.listen(port, function (req, res) {
     console.log("Listening to port " + port);
 })
+
